f555f6b366c049b6a7090ca4773c7c1a3634d4c8
iamkosgei <kosgeik2930@gmail.com>
2019-05-01 11:45:27 +0300

    first commit

---
+++ src/main/java/xyz/kosgei/notes/notes/NotesApplication.java
@@ -0,0 +1,13 @@
+package xyz.kosgei.notes.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApplication.class, args);
+	}
+
+}
